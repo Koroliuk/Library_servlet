ef980e618b7215bf91fe4d3a66ceee440cf48a40
asdasdasd <yana.korolyuk.097@gmail.com>
2021-05-24 20:52:43 +0300

    Update index.jsp

--- src/main/java/ua/training/controller/command/Login.java
+++ src/main/java/ua/training/controller/command/Login.java
@@ -1,5 +1,7 @@
 package ua.training.controller.command;
 
+import com.sun.net.httpserver.HttpContext;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class Login implements Command {
@@ -9,7 +11,7 @@ public class Login implements Command {
         String name = request.getParameter("name");
         String pass = request.getParameter("pass");
         System.out.println(name + " " + pass);
-        return "/login.jsp";
+        return "/user.jsp";
 //        String login = request.getParameter("login");
 //        String password = request.getParameter("password");
 //
