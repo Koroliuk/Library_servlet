fece82df3dcd6cb24dbc78a3b0283d4024f58f95
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-06-03 10:44:48 +0300

    Update services

--- src/main/java/ua/training/model/service/UserService.java
+++ src/main/java/ua/training/model/service/UserService.java
@@ -4,6 +4,7 @@ import ua.training.model.dao.DaoFactory;
 import ua.training.model.dao.UserDao;
 import ua.training.model.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public class UserService {
@@ -15,9 +16,35 @@ public class UserService {
         }
     }
 
-    public Optional<User> getUserByLogin(String login) {
+    public Optional<User> findByLogin(String login) {
         try (UserDao userDao = daoFactory.createUserDao()) {
             return userDao.findByLogin(login);
+        }
+    }
+
+    public List<User> findAll() {
+        try (UserDao userDao = daoFactory.createUserDao()) {
+            return userDao.findAll();
+        }
+    }
+
+    public void delete(long id) {
+        try (UserDao userDao = daoFactory.createUserDao()) {
+            userDao.delete(id);
+        }
+    }
+
+    public void blockUser(User user) {
+        try (UserDao userDao = daoFactory.createUserDao()) {
+            user.setBlocked(true);
+            userDao.update(user);
+        }
+    }
+
+    public void unBlockUser(User user) {
+        try (UserDao userDao = daoFactory.createUserDao()) {
+            user.setBlocked(false);
+            userDao.update(user);
         }
     }
 }
