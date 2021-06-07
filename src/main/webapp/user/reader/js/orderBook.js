152a8e14264540522451d6636818966c1176905d
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-06-07 15:35:26 +0300

    Add the ability to cancel and select a date to order in the reading room

--- src/main/webapp/user/reader/js/orderBook.js
+++ src/main/webapp/user/reader/js/orderBook.js
@@ -2,11 +2,5 @@ function chooseType() {
     const orderType = document.getElementById("orderType").value;
     document.getElementById('startDate').valueAsDate = new Date();
     document.getElementById('endDate').valueAsDate = new Date();
-    if (orderType === 'readingHole') {
-        document.getElementById('startDate').disabled = true;
-        document.getElementById('endDate').disabled = true;
-    } else {
-        document.getElementById('startDate').disabled = false;
-        document.getElementById('endDate').disabled = false;
-    }
+    document.getElementById('endDate').disabled = orderType === 'readingHole';
 }
