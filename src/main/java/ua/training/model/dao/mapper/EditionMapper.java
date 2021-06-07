23c56c91ca4e9ce0a7fc282bed45800a187c94cf
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-06-07 13:44:17 +0300

    Update bilingual date base processing

--- src/main/java/ua/training/model/dao/mapper/EditionMapper.java
+++ src/main/java/ua/training/model/dao/mapper/EditionMapper.java
@@ -19,7 +19,8 @@ public class EditionMapper implements ObjectMapper<Edition> {
     public Edition extractFromResultSetWithId(ResultSet resultSet, String id) throws SQLException {
         return new Edition.Builder()
                 .id(resultSet.getInt(id))
-                .name(resultSet.getString("edition_name"))
+                .name(resultSet.getString("edition_name_ua"))
+                .anotherName(resultSet.getString("edition_name_en"))
                 .build();
     }
 
